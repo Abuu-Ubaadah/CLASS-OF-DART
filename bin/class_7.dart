7580042627de09abfb7886ff36dce9a8e4d6dfec
Abuu Ubaadah <aldyfwkymhmd.gmail.com>
2025-02-03 23:16:10 +0700

    first commit

---
+++ bin/class_7.dart
@@ -0,0 +1,29 @@
+/* import 'package:class_7/class_7.dart' as class_7;
+
+void main(List<String> arguments) {
+  print('Hello world: ${class_7.calculate()}!');
+} */
+
+class Person {
+  String nama;
+  int usia;
+
+  // Konstruktor Untuk Inisialisasi Nama Dan Usia
+  Person(this.nama, this.usia);
+
+  // Fungsi Untuk Menampilkan Informasi
+  void tampilkanInfo() {
+    print('Nama: $nama, Usia: $usia Tahun');
+  }
+}
+
+void main() {
+  // Membuat Objek Person1
+  Person person1 = Person('Ahmad An-Najmiyy', 35);
+  person1.tampilkanInfo();  // Cetak: Nama: Ahmad An-Najmiyy, Usia: 25 tahun
+
+  // Membuat Objek Person2
+  Person person2 = Person('Ubaid Al-Jaabiriyy', 40);
+  person2.tampilkanInfo();  // Cetak: Nama: Ubaid Al-Jaabiriyy, Usia: 30 tahun
+}
+
---
+++ test/class_7_test.dart
@@ -0,0 +1,8 @@
+import 'package:class_7/class_7.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
